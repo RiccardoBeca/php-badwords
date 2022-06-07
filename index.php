66c1e96ef0731e2bc8f3f4524cd6b413c5364419
RiccardoBeca <riccardobeca@gmail.com>
2022-06-07 09:12:36 +0100

    html structure fixed

--- index.php
+++ index.php
@@ -19,18 +19,33 @@
  
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 
 <h2>Questo e' il primo testo:</h2>
      
 <h3> <?php echo  $testo ?> </h3>
-
+     
 <p>Il testo e' lungo <?php echo str_word_count("$testo") ?> parole.</p>
-
+     
 <h2>Il testo censurato e':</h2>
-
+     
 <h3><?php echo $censoredText ?></h3>
-
+     
 <p>La lunghezza del testo censurato e' di: <?php echo str_word_count("$censoredText") ?> parole.</p>
+  
+</body>
+</html>
+
+
+
 
 
 
